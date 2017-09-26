43ae9ea3f60a41e63bca296fa4c20ef5d586a047
wiznia <wiznia@gmail.com>
2017-09-25 22:53:08 -0300

    Updates to comments, styling

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -8,19 +8,42 @@ class Comments extends React.Component {
       email: this.email.value,
       comment: this.comment.value
     }
-
+    
     this.props.addComment(comment, this.props.post);
+    this.form.reset();
   }
+
+  renderComments(comment) {
+    if (comment === undefined) {
+      return (
+        <p>There are no comments yet, be the first one to comment!</p>
+      )
+    }
+    return (
+      <div>
+        {
+          comment.map((com, i) => {
+            return <div className="photogrid__item-comment-item" key={i}><span><a href={`mailto:${com.email}`}>{com.name}</a></span><p>{com.comment}</p></div>;
+          })
+        }
+      </div>
+    )
+  }
+
+  hideComments = () => {
+    this.commentItem.classList.remove('active');
+  };
 
   render() {
     return (
-      <div className="photogrid__item-comment">
-        <div className="photogrid__item-comment-area"></div>
-        <form onSubmit={(e) => this.addComment(e)}>
-          <input ref={(name) => {this.name = name}} type="text" name="name" placeholder="Name" />
-          <input ref={(email) => {this.email = email}} type="email" name="email" placeholder="Email" />
-          <textarea ref={(comment) => {this.comment = comment}} placeholder="Your comment"></textarea>
-          <button type="submit">Add Comment</button>
+      <div className="photogrid__item-comment" ref={(commentItem) => {this.commentItem = commentItem}}>
+        <a href="#close" onClick={this.hideComments} className="photogrid__item-comment-close">&#10799;</a>
+        <div className="photogrid__item-comment-area">{this.renderComments(this.props.comments)}</div>
+        <form className="photogrid__item-comment-form" ref={(form) => {this.form = form}} onSubmit={(e) => this.addComment(e)}>
+          <input className="photogrid__item-comment-form-item" ref={(name) => {this.name = name}} type="text" name="name" placeholder="Name" />
+          <input className="photogrid__item-comment-form-item" ref={(email) => {this.email = email}} type="email" name="email" placeholder="Email" />
+          <textarea className="photogrid__item-comment-form-item" ref={(comment) => {this.comment = comment}} placeholder="Your comment"></textarea>
+          <button className="button_bg photogrid__item-comment-form-item" type="submit">Add Comment</button>
         </form>
       </div>
     );
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -10,17 +10,17 @@ class Photo extends React.Component {
   };
 
   render() {
-    const { details, user } = this.props;
+    const { details, user, index, addComment, addLikes } = this.props;
     return (
       <div className="photogrid__item" ref={(photo) => {this.photo = photo}}>
         <img src={details.image} alt={details.title} />
         <h2 className="photogrid__item-title">{details.title}</h2>
         <p className="photogrid__item-desc">{details.desc}</p>
         <div className="photogrid__item-info">
-          <div onClick={this.props.addLikes} className={`photogrid__item-likes ${user && details.userLiked ? 'photogrid__item-likes_liked' : ''}`}><span role="img" aria-label="Likes">&#10084;</span><span>{details.likes}</span></div>
-          <div><span onClick={this.showComments} className="photogrid__item-comments" role="img" aria-label="Comments">&#10000;</span><span>{details.comments !== undefined ? Object.keys(details.comments).length : 0}</span></div>
+          <div onClick={addLikes} className={`photogrid__item-likes ${user && details.userLiked ? 'photogrid__item-likes_liked' : ''}`}><span role="img" aria-label="Likes">&#10084;</span><span>{details.likes}</span></div>
+          <div><span onClick={this.showComments} className="photogrid__item-comments" role="img" aria-label="Comments">&#10000;<span>{details.comments !== undefined ? Object.keys(details.comments).length : 0}</span></span></div>
         </div>
-        <Comments addComment={this.props.addComment} post={this.props.index} comments={this.props.details.comments} />
+        <Comments addComment={addComment} post={index} comments={details.comments} hideComments={this.hideComments} />
       </div>
     );
   }
